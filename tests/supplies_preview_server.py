e1d8c9ddac91d9e1d1a21039c68f75a381fe55d6
Максим <maxusachev7@gmail.com>
2026-09-23 18:30:51 +0300

    test: seed canonical stock in supplies preview

--- tests/supplies_preview_server.py
+++ tests/supplies_preview_server.py
@@ -3,6 +3,7 @@ import os
 import stage2_preview_server as fixture
 from app.services.supplies import SupplyEngine
 from app.services.sales_inventory import SalesInventory
+from app.services.warehouse_stock import default_warehouse_id, set_balance
 
 web = fixture.web
 products = [dict(external_product_id=str(i), name=name, external_sku='SUP-'+str(i), brand='Casio', stock=47, images=[], properties=[]) for i,name in [(90101,'Casio A168'),(90102,'Casio F91W'),(90103,'Casio smoke catalog'),(90104,'Casio smoke supply')]]
@@ -20,7 +21,7 @@ engine=SupplyEngine(fixture.fixture_catalog_database)
 engine.resolve_bitrix(products[1])
 card=engine.resolve_bitrix(products[0])
 with fixture.fixture_catalog_database.transaction() as c:
-    c.execute('UPDATE catalog_excel_products SET stock=3 WHERE id=?',(card['id'],))
+    set_balance(c, card['id'], default_warehouse_id(c), 3)
 sales=SalesInventory(fixture.fixture_catalog_database)
 sales.create_sale({'id':'supply-fixture-cancellation','source':'Tictactoy','order_number':'21096'},card['id'],1,100)
 sales.cancel_sale('supply-fixture-cancellation',reason='duplicate',user_name='Максим')
